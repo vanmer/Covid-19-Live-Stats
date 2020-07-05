dc79021592c04d51c7e3b6d85cfcc9eb8c080f7c
vanmer <60948275+vanmer@users.noreply.github.com>
2020-07-05 14:42:14 +0200

    select all elements

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,18 +1,10 @@
-// select all the elements
+// SELECT ALL ELEMENTS
 const country_name_element = document.querySelector(".country .name");
-const country_name_element = document.querySelector(".country .name");
-const country_name_element = document.querySelector(".country .name");
+const total_cases_element = document.querySelector(".total-cases .value");
+const new_cases_element = document.querySelector(".total-cases .new-value");
+const recovered_element = document.querySelector(".recovered .value");
+const new_recovered_element = document.querySelector(".recovered .new-value");
+const deaths_element = document.querySelector(".deaths .value");
+const new_deaths_element = document.querySelector(".deaths .new-value");
 
-
-
-
-
-
-total-cases
-recovered
-deaths
-
-value
-new-value
-
-axes_line_chart
+const ctx = document.getElementById("axes_line_chart").getContext("2d");
